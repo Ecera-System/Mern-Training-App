47e70a990e5fe090c767eeb417a8601975fc7963
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-12-27 00:14:17 +0530

    made changes in CourseAvailable.jsx

--- client/src/Pages/Profile/CourseAvailable/CourseAvailable.jsx
+++ client/src/Pages/Profile/CourseAvailable/CourseAvailable.jsx
@@ -2,28 +2,34 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import PageTitle from "../../Shared/PageTitle";
-import TermsPopup from "./TermsPopup"; // Import the new component
 import Spinner from "../../Shared/Spinner/Spinner";
 import { contextProvider } from "../../../Context/ContextProvider";
 import useGetEnrolledAndNotRefund from "../../../API/useGetEnrolledAndNotRefund";
 import useGetAllCourses from "../../../API/useGetAllCourses";
-import useGetRefundTerms from "../../../API/useGetRefundTerms";
+import TermsModal from "./TermsModal"; // Import the TermsModal component
 
 const CourseAvailable = () => {
   const [enrolledData] = useGetEnrolledAndNotRefund();
   const { showToast } = useContext(contextProvider);
   const [coursesData] = useGetAllCourses();
   const navigate = useNavigate();
-  const [refundTermsData] = useGetRefundTerms();
-  const [showTerms, setShowTerms] = useState(false);
-  const [showPopup, setShowPopup] = useState(false); // State to control the visibility of the popup
 
-  const openTermsPopup = () => {
-    setShowPopup(true);
+  // State variables for TermsModal
+  const [showTermsModal, setShowTermsModal] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  // Function to handle checkbox change in TermsModal
+  const handleCheckboxChange = () => {
+    setTermsAccepted(!termsAccepted);
   };
 
-  const closeTermsPopup = () => {
-    setShowPopup(false);
+  // Function to handle checkout button click
+  const handleCheckoutButtonClick = (data) => {
+    if (termsAccepted) {
+      navigate(`/course/checkout/${data?._id}`);
+    } else {
+      setShowTermsModal(true);
+    }
   };
 
   return (
@@ -36,29 +42,6 @@ const CourseAvailable = () => {
             <h2 className="text-2xl font-semibold md:text-start text-center">
               All Courses
             </h2>
-
-            <button
-              onClick={() => setShowTerms(!showTerms)}
-              className="bg-violet-600 hover:bg-violet-700 duration-300 text-white py-2.5 px-5 rounded-md mt-4"
-            >
-              {showTerms ? "Close Terms" : "Show Terms"}
-            </button>
-            {showTerms && (
-              <p>
-                Terms and condition - return window{" "}
-                {refundTermsData.returnWindow} days , registration fee{" "}
-                {refundTermsData.registrationFees}%
-              </p>
-            )}
-
-            {/* New button for Terms and Conditions */}
-            <button
-              onClick={openTermsPopup}
-              className="bg-violet-600 hover:bg-violet-700 duration-300 text-white py-2.5 px-5 rounded-md mt-4"
-            >
-              Terms and Conditions
-            </button>
-
             <div className="xl:w-full lg:w-[44rem] md:w-[40rem] w-[19rem] max-md:mx-auto grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 mt-8">
               {coursesData?.slice(0, 4).map((data) => {
                 const isEnrolled = enrolledData.some(
@@ -98,14 +81,15 @@ const CourseAvailable = () => {
                             Continue Learning
                           </button>
                         ) : (
-                          <button
-                            onClick={() =>
-                              navigate(`/course/checkout/${data?._id}`)
-                            }
-                            className="bg-violet-600 hover:bg-violet-700 duration-300 text-white py-2.5 px-5 rounded-md"
-                          >
-                            Checkout
-                          </button>
+                          <div className="mt-5">
+                            {/* Render TermsModal as a button when not enrolled */}
+                            <button
+                              onClick={() => handleCheckoutButtonClick(data)}
+                              className="bg-violet-600 hover:bg-violet-700 duration-300 text-white py-2.5 px-5 rounded-md"
+                            >
+                              Checkout
+                            </button>
+                          </div>
                         )}
                       </div>
                     </div>
@@ -117,13 +101,13 @@ const CourseAvailable = () => {
         )}
       </>
 
-      {/* Render the TermsPopup component if showPopup is true */}
-      {showPopup && (
-        <TermsPopup
-          onClose={closeTermsPopup}
-          refundTermsData={refundTermsData}
-        />
-      )}
+      {/* Render TermsModal with necessary props */}
+      <TermsModal
+        isOpen={showTermsModal}
+        toggleModal={() => setShowTermsModal(!showTermsModal)}
+        onCheckboxChange={handleCheckboxChange}
+        isChecked={termsAccepted}
+      />
     </div>
   );
 };
---
+++ client/src/Pages/Profile/CourseAvailable/TermsModal.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const TermsModal = ({ isOpen, toggleModal, onCheckboxChange, isChecked }) => {
+  return (
+    isOpen && (
+      <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+        <div className="bg-white p-8 rounded-lg">
+          <h2 className="text-2xl font-semibold mb-4">Terms and Conditions</h2>
+          <p>Your terms and conditions go here...</p>
+          <div className="mt-4">
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={onCheckboxChange}
+                className="mr-2"
+              />
+              I accept the terms and conditions
+            </label>
+          </div>
+          <div className="mt-6 flex justify-end">
+            <button
+              onClick={toggleModal}
+              className="bg-violet-600 hover:bg-violet-700 text-white py-2 px-4 rounded-md mr-2"
+            >
+              Close
+            </button>
+            <button
+              onClick={toggleModal}
+              className="bg-violet-600 hover:bg-violet-700 text-white py-2 px-4 rounded-md"
+              disabled={!isChecked}
+            >
+              Accept
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default TermsModal;
